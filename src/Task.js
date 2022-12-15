a376f23de3209fcf6210a4f58f11c68862bc735a
Asep <asepsuherman389@gmail.com>
2022-12-15 13:46:42 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Asep Suherman" // TODO: replace this
+const studentId = "FE2664682" // TODO: replace this
+
+export { url, studentName, studentId }
